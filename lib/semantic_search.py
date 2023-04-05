833bf58d1b3d3a0dd94ed60643346eb8b4f3ad06
erwanlenagard <erwan.lenagard@themetricsfactory.com>
2023-04-05 09:23:30 +0200

    Add files via upload

--- lib/semantic_search.py
+++ lib/semantic_search.py
@@ -39,6 +39,47 @@ pd.options.mode.chained_assignment = None
 
 openai.organization = st.secrets['openaiorg'] 
 openai.api_key = st.secrets['openaikey']
+from google.cloud.sql.connector import Connector, IPTypes
+import pymysql
+
+import sqlalchemy
+
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+    """
+    Initializes a connection pool for a Cloud SQL instance of MySQL.
+
+    Uses the Cloud SQL Python Connector package.
+    """
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
+
+    instance_connection_name = st.secrets['INSTANCE'] # e.g. 'project:region:instance'
+    db_user = st.secrets['USER']  # e.g. 'my-db-user'
+    db_pass = st.secrets['PASSWORD']  # e.g. 'my-db-password'
+    db_name = st.secrets['DATABASE']  # e.g. 'my-database'
+
+    ip_type = IPTypes.PRIVATE if "PRIVATE_IP" else IPTypes.PUBLIC
+
+    connector = Connector(ip_type)
+
+    def getconn() -> pymysql.connections.Connection:
+        conn: pymysql.connections.Connection = connector.connect(
+            instance_connection_name,
+            "pymysql",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+        )
+        return conn
+
+    pool = sqlalchemy.create_engine(
+        "mysql+pymysql://",
+        creator=getconn,
+        # ...
+    )
+    return pool
 
 # @st.cache_resource
 def connect_bdd():
